34e5ac0c12df96eeb096ac2af518ddd122b6ec7d
cn <cn@mycrobase.de>
2013-04-14 16:45:08 +0200

    One step back with metadata

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,6 @@ maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
 description      "Installs NSD and does basic configuration (no zone files!)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
-
-depends "git"
+version          "0.1.0"
 
 supports "debian"
