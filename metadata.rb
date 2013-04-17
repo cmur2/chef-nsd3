bd73fba7fd9cc5ca8a55fbfa2b2edd3013cc607d
cn <cn@mycrobase.de>
2013-04-17 13:19:43 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name             "nsd3"
 maintainer       "Christian Nicolai"
 maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
-description      "Installs NSD and does basic configuration (no zone files!)"
+description      "Installs NSD and does basic configuration including the copying of BIND compatible zone files."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "1.0.0"
 
 suggests "logrotate" # for nsd3::logrotate
 
