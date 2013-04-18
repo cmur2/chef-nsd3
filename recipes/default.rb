212c8b388baae433ba32065b99c88f57c702590d
cn <cn@mycrobase.de>
2013-04-18 18:52:22 +0200

    Restart on zone file change

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,11 +9,11 @@ execute "nsd-restart" do
 end
 
 file "/etc/nsd3/nsd.conf" do
-    content node.generate_nsd3_conf
-    owner "root"
-    group "root"
-    mode 00644
-    notifies :run, "execute[nsd-restart]"
+  content node.generate_nsd3_conf
+  owner "root"
+  group "root"
+  mode 00644
+  notifies :run, "execute[nsd-restart]"
 end
 
 node['nsd3']['zones'].each do |zone_name, data|
@@ -23,6 +23,7 @@ node['nsd3']['zones'].each do |zone_name, data|
     group "root"
     mode 00644
     cookbook node['nsd3']['file_cookbook'] if node['nsd3']['file_cookbook']
+    notifies :run, "execute[nsd-restart]"
   end
 end
 
