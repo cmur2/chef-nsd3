add3188c1452947f9cb9f5cb3f7c89f4063991ae
cn <cn@mycrobase.de>
2014-01-13 09:55:16 +0100

    Belong all config files via group to nsd user

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,7 +11,7 @@ end
 file "/etc/nsd3/nsd.conf" do
   content node.generate_nsd3_conf
   owner "root"
-  group "root"
+  group node['nsd3']['server']['username']
   mode 00644
   notifies :run, "execute[nsd-restart]"
 end
@@ -21,7 +21,7 @@ node['nsd3']['zones'].each do |zone_name, data|
   cookbook_file "/etc/nsd3/#{data['zonefile']}" do
     source data['zonefile']
     owner "root"
-    group "root"
+    group node['nsd3']['server']['username']
     mode 00644
     cookbook node['nsd3']['file_cookbook'] if node['nsd3']['file_cookbook']
     notifies :run, "execute[nsd-restart]"
